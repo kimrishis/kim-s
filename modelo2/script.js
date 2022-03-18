df5af92ef1599eb8a2641a67050e7886b7148c8f
kim <kimrishis@gmail.com>
2022-03-18 20:46:05 -0300

    add arquivos e var

--- modelo2/script.js
+++ modelo2/script.js
@@ -3,18 +3,23 @@ var msg = window.document.getElementById('msg')
 var img = window.document.getElementById('imagem')
 var data = new Date()
 var hora = data.getHours()
+var backgrounddia = '#e2cd9f'
+var backgroundtarde = '#b9846f'
+var backgroundnoite = '#515154'
+var bodystyle = document.body.style 
 msg.innerHTML = 'agora são $(hora) horas.'
 if (hora >= 0 && hora < 12 ) {
     //Bom dia!
-    img.src = 'pexels-jonathan-petersson-1237119.jpg'
-    document.body.style.background = '#e2cd9f'
+    img.src = 'dia.jpg'
+    bodystyle.background = backgrounddia 
+  
 } else if (hora >= 12 && hora <= 18) {
     //Boa tarde!
-    img.src = 'pexels-quang-nguyen-vinh-2132180.jpg'
-    document.body.style.background = '#b9846f'
+    img.src = 'tarde.jpg'
+bodystyle.background = backgroundtarde
 } else {
    // BOA NOITE!!!!
-   img.src = 'pexels-piccinng-3052361.jpg' 
-   document.body.style.background = '#515154'
+   img.src = 'noite.jpg' 
+bodystyle.background = backgroundnoite  
 }
 } 
