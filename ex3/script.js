71dc586a0ac09b6259fea6225adcb7eca862cfad
kim <kimrishis@gmail.com>
2022-03-29 18:14:49 -0300

    foto homem

--- ex3/script.js
+++ ex3/script.js
@@ -3,9 +3,9 @@ function verificar() {
     var ano = data.getFullYear()
     var fano = document.getElementById('txtano')
     var res = document.querySelector('div#res')
-    if (fano.value.length == 0 || fano.value > ano){
-        window.alert('[ERRO] verifique os dados e tente novamente')}
-        else{
+     if (fano.value.length == 0 || fano.value > ano) {
+            window.alert('[ERRO] verifique os dados e tente novamente')
+    } else {
             var fsex = document.getElementsByName('radsex')
             var idade = ano - Number(fano.value)
             var genero = ''
@@ -14,13 +14,16 @@ function verificar() {
             if (fsex[0].checked) {genero = 'homem'
                 if (idade >=0 && idade < 10){
                 // criança
-                img.setAttribute('src', 'bb h.jpg' )
+                img.setAttribute('src', 'bb-h.jpg')
             }   else if (idade >= 10 && idade < 21){
                 //jovem
+                img.setAttribute('src', 'menino-criança.jpg')
             }    else if (idade < 50){
                 // adulto
+                img.setAttribute('src', 'homem-adulto.jpg')
             }    else {
                 //idoso
+                img.setAttribute('src', 'homem-velho.jpg')
             }
         }else if (fsex[1].checked) {genero = 'mulher'
             if (idade >=0 && idade < 10){
@@ -34,8 +37,8 @@ function verificar() {
         }
     
     }
+    res.style.textAlign = 'center'
     res.innerHTML = `detectamos ${genero} com ${idade} anos`            
-}
-res.style.textAlign = 'center'
-res.innerHTML = `Detectamos ${genero} com ${idade} anos`
+    res.appendChild(img)
     }
+}
