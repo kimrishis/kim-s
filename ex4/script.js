9a8f20ac6c0426d312ae8f216a9d8c5af26be85e
kim <kimrishis@gmail.com>
2022-03-30 16:26:11 -0300

    final

--- ex4/script.js
+++ ex4/script.js
@@ -1,9 +1,33 @@
-function contar()
+function contar() {
 let ini = document.getElementById('txti')
 let fim = document.getElementById('txtf')
 let passo = document.getElementById('txtp')
-
-if (ini.value.length == 0 || passo.value.length ==0) { window.alert('[ERRO] faltam dados!')
+let res = document.getElementById('res') 
+if (ini.value.length == 0 || passo.value.length ==0) { 
+    res.innerHTML = 'impossivel contar!'
+    //window.alert('[ERRO] faltam dados!')
 } else {
-alert('TUDO OK!')
+    res.innerHTML = 'contando: <br>'
+    let i = Number(ini.value)
+    let f = Number(fim.value)
+    let p = Number (passo.value)
+    if (p <= 0) {
+     window.alert('passo invalido! Considerando PASSO 1') 
+     p = 1  
+    }
+    if (i < f) {
+        //contagem crescente
+    for (let c = i; c <= f; c += p) {
+        res.innerHTML += `${c} \u{1F449}`
+    }
+    } else {
+        //contagem regressiva
+        for (let c = i; c >=f; c -= p){
+            res.innerHTML += `${c} \u{1F449}`
+        }
+    }
+    res.innerHTML += `\u{1F3C1}`
+
 }
+}
+
