71dc90641bba9bffe8dd9f8d2217b22d0ef999fc
kim <kimrishis@gmail.com>
2022-03-30 13:35:09 -0300

    parte 1

---
+++ ex4/script.js
@@ -0,0 +1,9 @@
+function contar()
+let ini = document.getElementById('txti')
+let fim = document.getElementById('txtf')
+let passo = document.getElementById('txtp')
+
+if (ini.value.length == 0 || passo.value.length ==0) { window.alert('[ERRO] faltam dados!')
+} else {
+alert('TUDO OK!')
+}
