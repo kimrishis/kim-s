7185fe39b77358138f9d68194b9de1b2e24dccdc
kim <kimrishis@gmail.com>
2022-04-01 17:02:33 -0300

    com for

--- ex5/script.js
+++ ex5/script.js
@@ -7,12 +7,13 @@ function tabuada() {
         let n = Number(num.value)
         let c = 1
         tab.innerHTML = ''
-        while (c <=10) {
+        for (c=1; c<10; c++)
+        // while (c <=10) 
+        {
             let item = document.createElement('option')
             item.text = `${n} x ${c} = ${n*c}`
             item.value = `tab${c}`
             tab.appendChild(item)
-            c++
         }
     }
 }
