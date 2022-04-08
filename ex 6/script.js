40d46a505998ff8b2c123ef5fadf375de3b27dd3
kim <kimrishis@gmail.com>
2022-04-08 16:49:04 -0300

    final

--- ex 6/script.js
+++ ex 6/script.js
@@ -24,7 +24,7 @@ function adicionar() {
     if(isnumero(num.value) && !inlista(num.value, valores)){
         valores.push(Number(num.value))
         let item = document.createElement('option')
-        item.next = `valor ${num.value} adicionado`
+        item.innerText = `valor ${num.value} adicionado`
         lista.appendChild(item)
         
     } else { 
@@ -41,15 +41,17 @@ function finalizar() {
         let tot = valores.length
         let maior = valores[0]
         let menor = valores[0]
+        let soma = 0
         for(let pos in valores) {
+            soma = soma + valores[pos] 
             if (valores[pos] > maior)
                maior = valores[pos]
-            if (valores[por] < menor)
+            if (valores[pos] < menor)
                 menor = valores[pos]   
         }
-        media = soma / total
+        media = soma / tot
         res.innerHTML= ''
-        res.innerHTML += `<p>Ao todo, temos ${tot} numeros cadastrados.</p>`
+        res.innerHTML += `<p> Ao todo, temos ${tot} numeros cadastrados.</p>`
         res.innerHTML += `<p> maior valor informado foi ${maior}.</p>`
         res.innerHTML += `<p> somando todos os valores, temos ${soma}. </p>`
         res.innerHTML += `<p> A media dos valores digitados é ${media}. </p>`
